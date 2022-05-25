7cef7a9d0411dad4c5bfa4a4cbd22c59b60d9f30
ekoca <ebubekirkoca@hotmail.com>
2022-05-25 14:26:25 +0300

    "Bence oldu :) "

--- Ucak_Bilet_Fiyati_Hesap.java
+++ Ucak_Bilet_Fiyati_Hesap.java
@@ -17,104 +17,97 @@ public class Ucak_Bilet_Fiyati_Hesap {
         System.out.print("Lutfen yolculuk tipinizi seciniz(1:Tek Yon, 2:Gidis Donus) : ");
         tip=input.nextInt();
 
-            if(yas>0 && mesafe>0 && tip==1) {
+
+            if (yas > 0 && mesafe > 0 && tip == 1) {
 
                 tutar = mesafe * ucret;
-                System.out.print("Indirimsiz Tek Yon Tutari : " + tutar);
+                System.out.println("Indirimsiz Tek Yon Tutari : " + tutar);
 
-                if (yas < 12) {
+                    if (yas < 12) {
 
-                    indirim = tutar * 0.5;
-                    itut = tutar - indirim;
-                    System.out.print("12 yas indirimli tutar : " + itut);
+                        indirim = tutar * 0.5;
+                        itut = tutar - indirim;
+                        System.out.println("12 yas indirimli tutar : " + itut);
 
-                }
-                else if (12 < yas && yas <= 24) {
+                    } else if (12 < yas && yas <= 24) {
 
-                    indirim = tutar * 0.10;
-                    itut = tutar - indirim;
+                        indirim = tutar * 0.10;
+                        itut = tutar - indirim;
 
-                    System.out.print("12-24 yas indirimli tutar: " + itut);
+                        System.out.println("12-24 yas indirimli tutar: " + itut);
 
-                }
-                else if (24<yas && yas<65) {
+                    } else if (24 < yas && yas < 65) {
 
-                    System.out.print("Normal tutar: "+tutar);
+                        System.out.println("Normal tutar: " + tutar);
 
-                }
-                else if (yas>=65) {
+                    } else if (yas >= 65) {
 
-                    indirim=tutar*0.30;
-                    itut=tutar-indirim;
+                        indirim = tutar * 0.30;
+                        itut = tutar - indirim;
 
-                    System.out.print("65 yas indirimli tutar: "+itut);
+                        System.out.println("65 yas indirimli tutar: " + itut);
 
-                }
+                    }
 
-
-            }
-            else {
-
-                System.out.println("Tip1: Mesafe, yas, yolculuk tipi bilgilerinizi kontrol ediniz.");
 
             }
 
             // Aynı sistemin tip 2 için olan versiyonu sadece genel tutarı %20 indirimli yapacağım.
 
-        if(yas>0 && mesafe>0 && tip==2) {
+            if (yas > 0 && mesafe > 0 && tip == 2) {
 
-            tutar = mesafe * ucret;
+                tutar = mesafe * ucret;
 
-            System.out.print("Indirimsiz Gidis&Donus Tutari : " + tutar);
+                System.out.println("Indirimsiz Gidis&Donus Tutari : " + tutar);
 
-            if (yas < 12) {
+                    if (yas < 12) {
 
-                indirim = tutar * 0.5;
-                itut = tutar - indirim;
-                indirim=itut*0.2;
-                tutar=(itut-indirim)*2;
-                System.out.print("12 yas indirimli tutar : " + tutar);
+                        indirim = tutar * 0.5;
+                        itut = tutar - indirim;
+                        indirim = itut * 0.2;
+                        tutar = (itut - indirim) * 2;
+                        System.out.println("12 yas indirimli tutar : " + tutar);
+
+                    } else if (12 < yas && yas <= 24) {
+
+                        indirim = tutar * 0.10;
+                        itut = tutar - indirim;
+                        indirim = itut * 0.2;
+                        tutar = (itut - indirim) * 2;
+
+                        System.out.println("12-24 yas indirimli tutar: " + tutar);
+
+                    } else if (24 < yas && yas < 65) {
+
+                        tutar = tutar * 2;
+
+                        System.out.println("Normal tutar: " + tutar);
+
+                    } else if (yas >= 65) {
+
+                        indirim = tutar * 0.30;
+                        itut = tutar - indirim;
+                        indirim = itut * 0.2;
+                        tutar = (itut - indirim) * 2;
+
+                        System.out.println("65 yas indirimli tutar: " + tutar);
+                    }
+
 
             }
-            else if (12 < yas && yas <= 24) {
 
-                indirim = tutar * 0.10;
-                itut = tutar - indirim;
-                indirim=itut*0.2;
-                tutar=(itut-indirim)*2;
+            if(tip !=1 && tip!=2){
 
-                System.out.print("12-24 yas indirimli tutar: " + tutar);
+                System.out.println("Lutfen bilgilerinizi kontrol ediniz! ");
 
             }
-            else if (24<yas && yas<65) {
+                else if(yas>0 && mesafe<0 ){
+                System.out.println("Mesafe bilginizi kontrol ediniz!!!");
 
-                tutar=tutar*2;
-
-                System.out.print("Normal tutar: "+tutar);
-
-            }
-            else if (yas>=65) {
-
-                indirim=tutar*0.30;
-                itut=tutar-indirim;
-                indirim=itut*0.2;
-                tutar=(itut-indirim)*2;
-
-                System.out.print("65 yas indirimli tutar: "+tutar);
+                } else if (yas<0) {
+                    System.out.println("Yas bilginizi kontrol ediniz.");
 
             }
-
-
-        }
-
-        else {
-
-            System.out.println("Tip2: Mesafe, yas, yolculuk tipi bilgilerinizi kontrol ediniz.");
-
-        }
-
-
-
 
 
     }
